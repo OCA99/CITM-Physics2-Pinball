8d70212289a1b3f49feff9af5042ff900193d1f0
Oscar Cuatrecasas <o.cuatrix@gmail.com>
2020-11-15 14:08:14 +0100

    Couple of fixes

--- Pinball/ModulePlayer.cpp
+++ Pinball/ModulePlayer.cpp
@@ -47,25 +47,23 @@ bool ModulePlayer::CleanUp()
 
 update_status ModulePlayer::PreUpdate()
 {
-	if (App->ui->showWindow)
-		return UPDATE_CONTINUE;
-	if (App->input->GetKey(SDL_SCANCODE_LEFT) == KEY_REPEAT)
+	if (App->input->GetKey(SDL_SCANCODE_LEFT) == KEY_REPEAT && !App->ui->showWindow)
 		leftFlipperBody->body->ApplyTorque(-20.0f, true);
 	else
 		leftFlipperBody->body->ApplyTorque(15.0f, true);
 
-	if (App->input->GetKey(SDL_SCANCODE_RIGHT) == KEY_REPEAT)
+	if (App->input->GetKey(SDL_SCANCODE_RIGHT) == KEY_REPEAT && !App->ui->showWindow)
 		rightFlipperBody->body->ApplyTorque(20.0f, true);
 	else
 		rightFlipperBody->body->ApplyTorque(-15.0f, true);
 
-	if (App->input->GetKey(SDL_SCANCODE_DOWN) == KEY_REPEAT)
+	if (App->input->GetKey(SDL_SCANCODE_DOWN) == KEY_REPEAT && !App->ui->showWindow)
 	{
 		plunge->body->ApplyForceToCenter(b2Vec2(0, 250), true);
 		App->scene_intro->pikaAnimation.Step();
 	}
 
-	if (App->input->GetKey(SDL_SCANCODE_DOWN) == KEY_UP)
+	if (App->input->GetKey(SDL_SCANCODE_DOWN) == KEY_UP && !App->ui->showWindow)
 	{
 		App->audio->PlayFx(4, 0);
 		App->scene_intro->pikaAnimation.Reset();
